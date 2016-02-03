a2f439b88e8f355f34a89a107bcb50effa7d3a3f
Karen Ang <thekarenang@gmail.com>
2016-02-03 21:13:27 +0800

    Add more data (really) 🌝

--- src/engine/data.js
+++ src/engine/data.js
@@ -1,16 +1,19 @@
 var fsmHelper = require('./fsm')
 
-var listData = [[2, 3, 5, 7, 9, 11, 13, 17, 19, 21], ['weird', 'strange', 'amazing',
+var programmingLanguages = ['Ruby', 'JavaScript', 'Python', 'Swift',
+                            'C', 'Go', 'Java', 'Objective-C', 'Haskell', 'C++',
+                            'Lisp', 'Scheme', 'Scala', 'OCaml']
+
+var listData = [[2, 3, 5, 7, 11, 13, 17, 19, 23], ['weird', 'strange', 'amazing',
               'spectacular', 'beautiful', 'charming', 'inconceivable',
               'remarkable'], ['tricks', 'tips', 'ways', 'fixes', 'changes',
               'quick changes', 'lines of code', 'functions', 'uses of anonymous functions',
-              'uses of design patterns'], ['to',
+              'uses of design patterns', 'facts'], ['to',
               'which will'], ['completely', 'absolutely', 'mindblowingly',
               'totally'], ['improve', 'revolutionise', 'transform',
               'shake up'], ['your use of', 'the way you think about',
               'the way you code in', 'the way you do testing in'],
-              ['Ruby', 'JavaScript', 'Python', 'Swift', 'Node', 'C', 'Go', 'Java',
-              'Objective-C', 'Haskell', 'C++', 'Lisp', 'Scheme', 'Scala', 'OCaml'],
+              programmingLanguages,
               ['forever', 'like never before']]
 
 var whyData = [['Why you'], ['should be using', 'have to use'], ['map', 'filter',
@@ -22,6 +25,18 @@ var whyData = [['Why you'], ['should be using', 'have to use'], ['map', 'filter'
              'your new startup...', 'your server performance...'],
              ['You won\'t believe the answer!', 'It will blow your mind!',
              'This changes everything.', 'You can\'t imagine it.']]
+
+var xToYData = [['First you\'ll be'], ['shocked,', 'dismayed,', 'appalled,', 'furious,'],
+            ['then you\'ll be'], ['inspired', 'amazed', 'astonished', 'swooning',
+            'made speechless', 'bowled over', 'excited'], ['by'], ['this one-liner',
+            'these 2 lines of code', 'this simple trick'], ['in'], programmingLanguages]
+
+var howToData = [['It\'s possible!', 'You won\'t believe how this one developer did it.',
+                  'You\'ll have to see this one line of code to believe it!',
+                  'These 3 lines of code are all you need!'],
+                  ['How to'], ['speed up your app', 'scale your app', 'raise your MAU',
+                  'retain users', 'growth-hack your start-up', 'optimise your servers'],
+                  ['with this one-liner', 'using this simple trick']]
 
 var controversialData = [[]]
 
@@ -38,4 +53,5 @@ function constructStateChain (states) {
   return startState
 }
 
-export var variants = [constructStateChain(listData), constructStateChain(whyData)]
+var allData = [listData, whyData, xToYData, howToData]
+export var variants = allData.map(constructStateChain)
